88bf4e0fb0b60328beb955ad9f16d1f593b5ad85
Fantoca.Tako <fantoca@tako.co.id>
2023-11-08 14:35:10 +0700

    update 1.5.3

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: "resources/js/app.jsx",
+            detectTls: "http://karir.tako.co.id/",
             refresh: true,
         }),
         react(),
